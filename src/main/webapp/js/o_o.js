28568da7396d8e453922313f1305f65966fe6029
Irina Benediktovich <i.benediktovich@auckland.ac.nz>
2014-05-26 13:22:14 +1200

    Workaround for console absence for IE

--- src/main/webapp/js/o_o.js
+++ src/main/webapp/js/o_o.js
@@ -1,34 +1,60 @@
 (function () {
 
+	/*
+	 * Set up console if browser doesnt support it
+	 * (taken from twitter's source code)
+	 */
+
+	var noop = function () {};
+	var methods = [
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeStamp', 'trace', 'warn'
+	];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
+
+	var method;
+	while (length--) {
+		method = methods[length];
+
+		// Only stub undefined methods.
+		if (!console[method]) {
+			console[method] = noop;
+		}
+	}
+
+	var devMode = true;
+	if (devMode && console['log']===noop){
+		console['log'] = function(message){
+			var errorbox = document.createElement('div');
+			errorbox.className = 'fancyerror';
+			errorbox.innerHTML = 'JS: <span class="errmsg" >' + message.replace('<', '&lt;').replace('>', '&gt;') + '</span>';
+			document.body.appendChild(errorbox);
+		}
+	}
+
+
+	/*
+	 * Functions here
+	 */
+
 	var is_IE = function(){
 		var myNav = navigator.userAgent.toLowerCase();
 		return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
 	};
 
-	var errorInIE = function(message, url, linenumber) {
-		var errorbox = document.createElement('div');
-		errorbox.className = 'fancyerror';
-		errorbox.innerHTML = 'JS: <span class="errmsg" >' +
-			message.replace('<', '&lt;').replace('>', '&gt;') +
-			'</span><br>line number: ' + linenumber +
-			'<br>located: ' + url;
-		document.body.appendChild(errorbox);
-	};
-
 	var reportError = function(message, file, line, column, errorObj) {
-		var ie = is_IE();
-		if (ie && ie < 9){	// cant console.log()
-			errorInIE(message, file, line);
-			return true;
-		}else{
-			console.log(message);
-			if (file || line || column) {
-				console.log("at: " + file + ":" + line + ":" + column);
-			}
-			if (errorObj !== undefined) //so it won't blow up in the rest of the browsers
-				console.log('Error: ' + errorObj.stack);
-			return false;
+		var msg = message;
+		if (file || line || column) {
+			msg += "\nat: " + file + ":" + line + ":" + column;
 		}
+		if (errorObj !== undefined) //so it won't blow up in the rest of the browsers
+			msg += '\nStacktrace:\n' + errorObj.stack;
+
+		console.log(msg);
+		return false;
 	};
 
 	var sendError = function(message, file, line, column, errorObj){
@@ -53,16 +79,13 @@
 var OOO = OOO || {};
 OOO.makeError = function () {
 	var a;
-	a.u = "5"
+	a.u = "5";
 };
 
 OOO.sendError = function (){
 	try {
 		OOO.makeError();
 	}catch (e){
-		//console.log(e.message);
-		//console.log(e.stack);
-
 		if (window.XMLHttpRequest) {
 			var xhr = new XMLHttpRequest();
 			var scripturl = '/reportjserror/log';
