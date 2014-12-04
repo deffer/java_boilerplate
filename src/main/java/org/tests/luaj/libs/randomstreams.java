cea8dca62e4303a0469a37f40dcb397cb2a0d00f
deffer <irina.benediktovich@gmail.com>
2014-12-04 16:54:39 +1300

    added global varargs function

--- src/main/java/org/tests/luaj/libs/randomstreams.java
+++ src/main/java/org/tests/luaj/libs/randomstreams.java
@@ -1,31 +1,53 @@
 package org.tests.luaj.libs;
 
+import org.luaj.vm2.LuaFunction;
 import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.TwoArgFunction;
+import org.luaj.vm2.lib.VarArgFunction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class randomstreams extends TwoArgFunction {
 
 	public randomstreams(){
-		System.out.println("Library: Constructor called");
+
 	}
 
 
 	@Override
 	public LuaValue call(LuaValue modname, LuaValue env) {
-		System.out.println("Library: Init called");
 		LuaValue library = tableOf();
-		library.set( "rs", new rs() );
-		//library.set( "cosh", new cosh() );
-		env.set( "randomstreams", library );
+		library.set( "num", new num() );
+		env.set( "rstreams", library );
+
+		// also add a function
+		LuaFunction func = new RandomPcStream();
+		env.set("random_pc_stream", func);
 		return library;
 	}
 
-	public static class rs extends OneArgFunction {
+	public static class num extends OneArgFunction {
 
 		@Override
 		public LuaValue call(LuaValue arg) {
 			return LuaValue.valueOf(arg.checkint()*100);
+		}
+	}
+
+	public static class RandomPcStream extends VarArgFunction {
+
+		@Override
+		public Varargs invoke(Varargs args) {
+			List<LuaValue> result = new ArrayList<>();
+			int count = args.arg(1).checkint();
+			for (int i =0; i<count; i++){
+				int val = (int)(Math.random()*100);
+				result.add(LuaValue.valueOf(val));
+			}
+			return LuaValue.varargsOf(result.toArray(new LuaValue[result.size()]));
 		}
 	}
 }
--- src/main/resources/streams.lua
+++ src/main/resources/streams.lua
@@ -2,9 +2,10 @@
 --print (f)
 
 local testStreams = function(val)
-    print(randomstreams)
-    print(randomstreams.rs)
-    print(randomstreams.rs(val))
+    print(rstreams)
+    print(rstreams.num)
+    print(rstreams.num(val))
 end
 
 testStreams(5);
+print (random_pc_stream(4))
